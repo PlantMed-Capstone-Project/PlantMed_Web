26cf6907663b0ea243038927c67cfcfbe88ce174
buitannguyen2612 <buitannguyen26122001@gmail.com>
2024-01-10 10:33:12 +0700

    fix header

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,41 @@ import HomePage from 'pages/HomePage'
 import PlantPage from 'pages/Plant/PlantPage'
 import BlogPage from 'pages/BlogPage/BlogPage'
 import { Box } from '@mui/material'
-import GoogleFontLoader from 'react-google-font-loader';
+import GoogleFontLoader from 'react-google-font-loader'
 import DetailPage from 'pages/DetailPage/DetailPage'
+import Header from 'components/Header/Header'
 
 function App() {
-  return (
-    <Box component="section" sx={{ display: "flex", justifyContent: "center", width: "100%" }}>
-      <GoogleFontLoader
-        fonts={[
-          {
-            font: 'Roboto',
-            weights: [200, 400, 500, 700],
-          },
-        ]}
-      />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/plant" element={<PlantPage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/detail" element={<DetailPage />} />
-        </Routes>
-      </BrowserRouter>
-    </Box>
-
-
-  )
+    return (
+        <Box
+            component="section"
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                width: '100%',
+                alignItems: 'center',
+            }}
+        >
+            <GoogleFontLoader
+                fonts={[
+                    {
+                        font: 'Roboto',
+                        weights: [200, 400, 500, 700],
+                    },
+                ]}
+            />
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/plant" element={<PlantPage />} />
+                    <Route path="/blog" element={<BlogPage />} />
+                    <Route path="/detail" element={<DetailPage />} />
+                </Routes>
+            </BrowserRouter>
+        </Box>
+    )
 }
 
 export default App
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -12,44 +12,143 @@ import imageDayLeo from 'Images/heroSen.jpg'
 import imageBachBo from 'Images/heroSi.jpg'
 
 export default function HomePage() {
-
     const spacingTop = 5
     const plants = [
-        { id: 1, title: 'Cầu Kỳ Tử', image: imgDemo, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
-        { id: 2, title: 'Cầu Kỳ Tử', image: imgDemo, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
-        { id: 3, title: 'Cầu Kỳ Tử', image: imgDemo, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
-        { id: 4, title: 'Cầu Kỳ Tử', image: imgDemo, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
-        { id: 5, title: 'Cầu Kỳ Tử', image: imgDemo, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
-        { id: 6, title: 'Cầu Kỳ Tử', image: imgDemo, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
-        { id: 7, title: 'Cầu Kỳ Tử', image: imgDemo, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
-        { id: 8, title: 'Cầu Kỳ Tử', image: imgDemo, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
-        { id: 9, title: 'Cầu Kỳ Tử', image: imgDemo, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
-        { id: 10, title: 'Cầu Kỳ Tử', image: imgDemo, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
+        {
+            id: 1,
+            title: 'Cầu Kỳ Tử',
+            image: imgDemo,
+            description:
+                'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        },
+        {
+            id: 2,
+            title: 'Cầu Kỳ Tử',
+            image: imgDemo,
+            description:
+                'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        },
+        {
+            id: 3,
+            title: 'Cầu Kỳ Tử',
+            image: imgDemo,
+            description:
+                'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        },
+        {
+            id: 4,
+            title: 'Cầu Kỳ Tử',
+            image: imgDemo,
+            description:
+                'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        },
+        {
+            id: 5,
+            title: 'Cầu Kỳ Tử',
+            image: imgDemo,
+            description:
+                'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        },
+        {
+            id: 6,
+            title: 'Cầu Kỳ Tử',
+            image: imgDemo,
+            description:
+                'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        },
+        {
+            id: 7,
+            title: 'Cầu Kỳ Tử',
+            image: imgDemo,
+            description:
+                'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        },
+        {
+            id: 8,
+            title: 'Cầu Kỳ Tử',
+            image: imgDemo,
+            description:
+                'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        },
+        {
+            id: 9,
+            title: 'Cầu Kỳ Tử',
+            image: imgDemo,
+            description:
+                'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        },
+        {
+            id: 10,
+            title: 'Cầu Kỳ Tử',
+            image: imgDemo,
+            description:
+                'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        },
     ]
 
     const searchProducts = [
-        { id: 1, name: 'Product 1', image: imageBachBo, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
-        { id: 2, name: 'Product 2', image: imageDayLeo, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
-        { id: 3, name: 'Product 3', image: imageBachBo, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
-        { id: 4, name: 'Product 4', image: imageDayLeo, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
-        { id: 5, name: 'Product 5', image: imageBachBo, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
+        {
+            id: 1,
+            name: 'Product 1',
+            image: imageBachBo,
+            description:
+                'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        },
+        {
+            id: 2,
+            name: 'Product 2',
+            image: imageDayLeo,
+            description:
+                'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        },
+        {
+            id: 3,
+            name: 'Product 3',
+            image: imageBachBo,
+            description:
+                'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        },
+        {
+            id: 4,
+            name: 'Product 4',
+            image: imageDayLeo,
+            description:
+                'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        },
+        {
+            id: 5,
+            name: 'Product 5',
+            image: imageBachBo,
+            description:
+                'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        },
     ]
 
     return (
-        <Box component="section" sx={{ width: "90rem", display: "flex", flexDirection: "column", alignItems: "center" }}>
-            <Header />
+        <Box
+            component="section"
+            sx={{
+                width: '90rem',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
             <Heros />
             <Sologan />
-            <FeaturedSearch title='TÌM KIẾM NỔI BẬT' data={searchProducts} />
+            <FeaturedSearch title="TÌM KIẾM NỔI BẬT" data={searchProducts} />
             <Stack
                 direction="column"
                 alignItems="center"
                 width="100%"
                 height="106.94rem"
                 mt="6.38rem"
-                sx={{ backgroundColor: "#F4FFEB" }}
+                sx={{ backgroundColor: '#F4FFEB' }}
             >
-                <DiversityPlant title='ĐA DẠNG CÁC LOẠI THỰC VẬT' data={plants} />
+                <DiversityPlant
+                    title="ĐA DẠNG CÁC LOẠI THỰC VẬT"
+                    data={plants}
+                />
                 <SpecialFeature />
             </Stack>
             <Quantity />
