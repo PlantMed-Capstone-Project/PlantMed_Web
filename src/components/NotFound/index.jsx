519cda4826ee5ef456c25673c411f8ae93b89ccf
buitannguyen2612 <buitannguyen26122001@gmail.com>
2024-01-22 16:10:52 +0700

    adding more component

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,30 +1,44 @@
-import { Box, CardMedia } from '@mui/material'
+import { Box, Button, CardMedia, Typography } from '@mui/material'
 import React from 'react'
 import notfoundImage from 'Images/notfound.png'
+import * as styleMui from './NotFound.styled'
+import { motion } from 'framer-motion'
+import { useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
+    const navigate = useNavigate()
+
+    const gotoHomePage = () => {
+        navigate({
+            pathname: '/',
+        })
+    }
+
     return (
-        <Box
-            sx={{
-                height: '100vh',
-                width: '100%',
-                display: 'flex',
-                justifyContent: 'center',
-            }}
-        >
-            <Box sx={{ width: '77.125rem', height: '38.5625rem' }}>
+        <styleMui.container>
+            <styleMui.containerImg>
                 <CardMedia
                     component="img"
                     image={notfoundImage}
                     alt="Khong tim thay"
-                    style={{
+                    sx={{
                         height: '100%',
                         width: '100%',
                         objectFit: 'cover',
                     }}
                 />
-            </Box>
-        </Box>
+            </styleMui.containerImg>
+            <styleMui.text>KHÔNG TÌM THẤY TRANG</styleMui.text>
+            <styleMui.btn
+                component={motion.div}
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+                onClick={() => gotoHomePage()}
+            >
+                Quay về trang chủ
+            </styleMui.btn>
+        </styleMui.container>
     )
 }
 
