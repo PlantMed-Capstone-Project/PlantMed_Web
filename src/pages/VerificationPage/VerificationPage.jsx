492cd8b17a79ef14c74cb595ff61ac0b041c90ae
HxYNQ2002 <huynqce161074@fpt.edu.vn>
2024-02-05 21:25:04 +0700

    fix: change motion.div to component

--- src/pages/VerificationPage/VerificationPage.jsx
+++ src/pages/VerificationPage/VerificationPage.jsx
@@ -1,21 +1,38 @@
 import VerificationForm from 'components/VerificationForm/VerificationForm'
 import * as styleMui from './VerificationPage.styled'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import { motion } from 'framer-motion'
 
 function VerificationPage() {
+    const location = useLocation()
+    const searchParams = new URLSearchParams(location.search)
+
+    //trigger animation khi scrollY
+    useEffect(() => {
+        let checkNav = true
+
+        if (checkNav) {
+            window.scrollTo(0, 100)
+        }
+
+        // clean up func
+        return () => {
+            checkNav = false
+        }
+    }, [searchParams])
     return (
         <styleMui.container>
             <styleMui.Background>
-                <motion.div
+                <styleMui.formContainer
+                    component={motion.div}
                     initial={{ opacity: 0, x: '-30%' }}
                     animate={{ opacity: 1, x: '0%' }}
                     exit={{ opacity: 0, x: '30%' }}
                     transition={{ duration: 0.5, ease: 'easeOut' }}
                 >
-                    <styleMui.formContainer>
-                        <VerificationForm />
-                    </styleMui.formContainer>
-                </motion.div>
+                    <VerificationForm />
+                </styleMui.formContainer>
             </styleMui.Background>
         </styleMui.container>
     )
