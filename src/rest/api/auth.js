84419d5d9d298c8ffb3d07933b11deed0fe1fbeb
HxYNQ2002 <huynqce161074@fpt.edu.vn>
2024-03-10 22:40:21 +0700

    working on api

--- src/rest/api/auth.js
+++ src/rest/api/auth.js
@@ -1,6 +1,6 @@
 import Client from 'rest/baseClient'
 
-const http = new Client('https://neutral-weevil-formerly.ngrok-free.app/api/v1/')
+const http = new Client()
 
 export const login = async (payload) => {
     return http.post('auth/login', payload)
