7801a357b7836b9d129b2d8244e783c84108d4dc
HxYNQ2002 <huynqce161074@fpt.edu.vn>
2024-03-20 18:34:26 +0700

    fix the liked blog

--- src/pages/MyBlog/MyBlog.styled.js
+++ src/pages/MyBlog/MyBlog.styled.js
@@ -47,26 +47,3 @@ export const statusTab = styled(Tab)(() => ({
     height: '2rem',
     textTransform: 'capitalize',
 }))
-
-export const blogCardList = styled(Stack)(() => ({
-    width: '100%',
-    height: 'calc(100vh - 12rem)',
-    justifyContent: 'flex-start',
-    padding: '1rem 0 2rem 4rem',
-    overflowY: 'auto',
-    '&::-webkit-scrollbar': {
-        width: '0',
-    },
-    scrollbarWidth: 'none',
-}))
-
-export const loadingText = styled(Typography)(() => ({
-    color: '#214400',
-    fontWeight: '500',
-    fontSize: '1.5rem',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    textTransform: 'inherit',
-}))
