ff6a2a78594dc405fd99eb2682ce9ec62f1b928f
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2024-04-04 16:12:08 +0700

    chore: remove warning

--- src/pages/ApprovalPage/index.jsx
+++ src/pages/ApprovalPage/index.jsx
@@ -1,4 +1,4 @@
-import { Box, Stack } from '@mui/material'
+import { Stack } from '@mui/material'
 import { blogAction } from 'app/reducers/blog'
 import { BlogApprovalCard } from 'components/BlogApproval'
 import HeroBlog from 'components/HeroBlog/HeroBlog'
