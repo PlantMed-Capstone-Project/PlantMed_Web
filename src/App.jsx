3c7da2fab63143dbf12d4e99bb05ed046d388717
Hưng Phú <80585279+phunlh2001@users.noreply.github.com>
2024-04-11 17:35:46 +0700

    chore: remove logger

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,6 @@ function App() {
             const response = await getAll()
             const data = response.data
             storePlantSuccessful(data)
-            console.log(data)
         } catch (error) {
             console.log(error)
             storeBlogFailed()
